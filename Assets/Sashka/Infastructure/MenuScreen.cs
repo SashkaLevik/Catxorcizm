e48b841f5f1cd800976941ec311ef69cd476f067
SashkaLevik <levik123@yandex.ru>
2023-05-30 16:58:35 +0300

    LoadLevel

---
+++ Assets/Sashka/Infastructure/MenuScreen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Assets.Sashka.Infastructure
+{
+    public class MenuScreen : MonoBehaviour
+    {
+        [SerializeField] private GameObject _menuScreen;
+        [SerializeField] private Button _startButton;
+
+        public event UnityAction ButtonPressed;
+
+        private void OnEnable()
+        {
+            _startButton.onClick.AddListener(OnStartButton);
+        }
+
+        private void OnStartButton()
+        {
+            _menuScreen.SetActive(false);
+            ButtonPressed?.Invoke();
+        }
+    }
+}
